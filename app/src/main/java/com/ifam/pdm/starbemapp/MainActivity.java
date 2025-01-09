7db554f8bace224437fa470de0acddeea09102f8
matheusasc <matheus.assuncao994@gmail.com>
2025-01-08 20:19:52 -0400

    adicionado bottom navigation e inicio da tela home

---
+++ app/src/main/java/com/ifam/pdm/starbemapp/MainActivity.java
@@ -0,0 +1,60 @@
+package com.ifam.pdm.starbemapp;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener  {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.flFragment, new HomeFragment())
+                    .commit();
+        }
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+
+    HomeFragment homeFragment = new HomeFragment();
+    HistoricoFragment historicoFragment = new HistoricoFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, homeFragment)
+                        .commit();
+                return true;
+
+            case R.id.history:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, historicoFragment)
+                        .commit();
+                return true;
+        }
+
+        return false;
+    }
+}
