3471867d3edeeb2a42207b658ec26de1e0a9f2e4
Luis Gabriel <gabrielset20@outlook.com>
2025-01-28 12:52:52 -0400

    imagem de perfil + legendas para gráfico

--- app/src/main/java/com/ifam/pdm/starbemapp/model/AtividadeDao.java
+++ app/src/main/java/com/ifam/pdm/starbemapp/model/AtividadeDao.java
@@ -4,10 +4,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
-
-import com.ifam.pdm.starbemapp.model.BDAtividades;
-
 import java.util.ArrayList;
 import java.util.List;
 
